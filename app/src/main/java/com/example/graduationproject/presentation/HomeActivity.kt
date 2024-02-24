03f5d52830bf3d53bd4d5e808478bca2f467959d
Panda <al_1303@tut.by>
2024-02-25 01:11:08 +0300

    added layout activity home

---
+++ app/src/main/java/com/example/graduationproject/presentation/HomeActivity.kt
@@ -0,0 +1,7 @@
+package com.example.graduationproject.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.graduationproject.R
+
+class HomeActivity: AppCompatActivity (R.layout.activity_home) {
+}
